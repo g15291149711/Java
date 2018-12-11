06511b059216f627f7f30d7c3b435239d1024281
g15291149711 <1519284436@qq.com>
2018-12-11 22:11:17 +0800

    语句

---
+++ java结构与逻辑控制/Test.java
@@ -0,0 +1,41 @@
+public class Test{
+   // public static void main(String[]args) throws Exception {
+         // double date = 1.1;
+        // System.out.println(date * 2);
+        // System.out.println(1.1 * 1.1); 
+        // int line = 10;
+        // for(int x = 0; x < line; x++){
+        //     for(int y = 0; y < line - x; y++){
+        //     System.out.print(" ");
+        //     }
+        //     for(int z = 0; z < x; z++){
+        //         System.out.print("* ");
+        //     }
+        // System.out.println();
+        // //switch case 语句
+        // System.out.println("请输入字符.....");
+        // char x = (char) System.in.read();
+        // switch(x){
+        //     case 'a':{
+        //         System.out.println("我是猪");
+        //         break;
+        //     }
+        //     case 'b':{
+        //         System.out.println("你是猪");
+        //         break;
+        //     }
+        //     case 'c':{
+        //         System.out.println("都是猪");
+        //         break;
+        //     }
+        //     default:{
+        //         System.out.println("都不是猪");
+        //         break;
+        //     }
+        // }
+        //for - each循环
+        // for(String str : args){
+        //     System.out.println(str);
+        // }
+    //}
+}
